9f699e8408a1cdd45d3779eb6792ef6b6a39938a
elementl-devtools <devtools@elementl.com>
2019-11-19 12:39:25 -0800

    update image tag version

--- .buildkite/images/docker/windows/build.ps1
+++ .buildkite/images/docker/windows/build.ps1
@@ -5,15 +5,14 @@ $py36 = "3.6.8"
 $py37 = "3.7.4"
 
 $VERSIONS = @($py27, $py35, $py36, $py37)
-$IMAGE_VERSION="v5.1"
+$IMAGE_VERSION="v6"
 
 foreach ($version in $VERSIONS) {
-    $name = ("{0}{1}" -f $version.substring(0,1), $version.substring(2,1))
     $major_version = $version.substring(0,1)
     docker build . `
         --no-cache `
         --build-arg PYTHON_VERSION=$version `
         --build-arg PYTHON_MAJOR_VERSION=$major_version `
         --target dagster-integration-image `
-        -t "dagster/buildkite-integration:py${name}-windows-${IMAGE_VERSION}"
+        -t "dagster/buildkite-integration:py${version}-windows-${IMAGE_VERSION}"
 }
--- .buildkite/images/docker/windows/push.ps1
+++ .buildkite/images/docker/windows/push.ps1
@@ -9,9 +9,8 @@ $AWS_ACCOUNT_ID=$env:AWS_ACCOUNT_ID
 $IMAGE_VERSION="v6"
 
 foreach ($version in $VERSIONS) {
-    $name = ("{0}{1}" -f $version.substring(0,1), $version.substring(2,1))
-    docker tag "dagster/buildkite-integration:py${name}-windows-${IMAGE_VERSION}" `
-        "${AWS_ACCOUNT_ID}.dkr.ecr.us-west-1.amazonaws.com/buildkite-integration:py${name}-windows-${IMAGE_VERSION}"
+    docker tag "dagster/buildkite-integration:py${version}-windows-${IMAGE_VERSION}" `
+        "${AWS_ACCOUNT_ID}.dkr.ecr.us-west-1.amazonaws.com/buildkite-integration:py${version}-windows-${IMAGE_VERSION}"
 
-    docker push "${AWS_ACCOUNT_ID}.dkr.ecr.us-west-1.amazonaws.com/buildkite-integration:py${name}-windows-${IMAGE_VERSION}"
+    docker push "${AWS_ACCOUNT_ID}.dkr.ecr.us-west-1.amazonaws.com/buildkite-integration:py${version}-windows-${IMAGE_VERSION}"
 }
